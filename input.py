995adeac0ff3396156a8284f25b99df5cea3d0c8
binysebastian <48433476+binysebastian@users.noreply.github.com>
2020-07-02 13:33:57 +0530

    Update input.py

--- input.py
+++ input.py
@@ -2,8 +2,8 @@ phasebp='n' #'n','y'
 scales=[0,5,15,50]#[0,5,15,50]
 weighting='briggs'#options -'natural','briggs'
 midchan=900
-usepacal='n' #option y/n keep it n if you don't wan to use polarized cal for estimating instrumental polarizationpolarization calibrator so
+usepacal='n' #option y/n keep it n if you don't wan to use polarized cal for estimating instrumental polarization+instead unpolarized calibrator will be used.
 usemidchan='n'#option y/n keep it n if you don't wan to use this midchan
 flagfile='/home/biny/FLAG.FLG'
 useflagfile='n'#option y/n keep it n if you don't wan to use this midchan
-pr=2 # AIPS TV number
+
